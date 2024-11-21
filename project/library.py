26d52b5e1a09ba1355e667ac0f47449968ff9e21
Tryd0g0lik <work80@mail.ru>
2024-11-21 13:03:53 +0700

    [master]: update

--- project/library.py
+++ project/library.py
@@ -9,7 +9,15 @@ class Library:
                  title: str,
                  author: str,
                  year: int,
-                 status="в наличии"):
+                 status="в наличии") -> None:
+        """
+        TODO: Add a new bork
+        :param title: str. This is a name book.
+        :param author: str.
+        :param year: int. This is a year when this is book was created.
+        :param status: is "в наличии" or "выдана"
+        :return:
+        """
         new_book = Books(
             title=title,
             author=author,
@@ -19,4 +27,6 @@ class Library:
         self.session.add(new_book)
         self.session.commit()
         print(f"Книга '{title}' добавлена.")
-        
+       
+    def remove_book(self, book_id: int) -> None:
+        pass
